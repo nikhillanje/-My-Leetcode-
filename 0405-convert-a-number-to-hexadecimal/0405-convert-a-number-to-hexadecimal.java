4513f2e7d6407db22669a466d3cea8dc8a925da8
Nikhil Lanje <nikhillanje00@gmail.com>
2026-07-13 13:53:35 +0530

    Time: 0 ms (100%), Space: 42.6 MB (62.34%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toHex(int num) {
+
+        String hex = Integer.toHexString(num);
+
+        // int ans = Integer.parseInt(hex);
+
+        return hex;
+        
+    }
+}
