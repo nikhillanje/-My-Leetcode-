a4b26da46d2431dddbafc1a01195456149b48ed7
Nikhil Lanje <nikhillanje00@gmail.com>
2026-08-02 23:18:58 +0530

    Time: 6 ms (22.55%), Space: 47 MB (10.85%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+
+        int n = piles.length;
+
+        int[][] dp = new int[n][n];
+
+        // Fills the Diagonal With nums element
+        for(int i = 0 ; i < n; i++){
+            dp[i][i] = piles[i];
+        }
+
+        // Fills the DP Table
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+
+                dp[i][j] = Math.max(
+                        piles[i] - dp[i + 1][j],
+                        piles[j] - dp[i][j - 1]
+                );
+            }
+        }
+
+
+        return dp[0][n - 1] >= 0;
+        
+    }
+}
