3e6142ae18e0a14a9b3b83dea9c623a820ef2f10
Nikhil Lanje <nikhillanje00@gmail.com>
2026-08-23 22:40:21 +0530

    Time: 7 ms (79.56%), Space: 46.8 MB (83.94%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                leftQ++;
+            } else {
+                leftSum += ch - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                rightQ++;
+            } else {
+                rightSum += ch - '0';
+            }
+        }
+
+        // Odd number of '?' -> Alice gets an extra move
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // Bob wins only when exact balancing is possible
+        return 2 * (leftSum - rightSum) != 9 * (rightQ - leftQ);
+    }
+}
