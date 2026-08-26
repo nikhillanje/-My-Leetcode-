ad245ebc3264e8ad9c323db1727283e1edc5e9d9
Nikhil Lanje <nikhillanje00@gmail.com>
2026-08-26 10:29:25 +0530

    Time: 6 ms (10.07%), Space: 44.3 MB (28.86%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,9 +1,14 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
 
+
+
         StringBuilder sb = new StringBuilder();
 
         StringBuilder ans = new StringBuilder();
+
+
+
 
         for (int i = 0; i < s.length(); i++) {
 
@@ -25,17 +30,13 @@ class Solution {
             }
 
 
-        
-        // Only consider substrings containing exactly k ones
             if (count == k) {
 
-                if (ans.length() == 0 ||
-                    sb.length() < ans.length() ||
-                    (sb.length() == ans.length() &&
-                     sb.toString().compareTo(ans.toString()) < 0)) {
+                if (ans.length() == 0 || sb.length() < ans.length() || (sb.length() == ans.length() && sb.toString().compareTo(ans.toString()) < 0)) {
 
                     ans.setLength(0);
                     ans.append(sb);
+
                 }
             }
 
