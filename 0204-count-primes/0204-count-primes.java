49bfa99cdde0243b8a6735e0e468dd5242c4b0ed
Nikhil Lanje <nikhillanje00@gmail.com>
2026-09-06 22:38:27 +0530

    Time: 620 ms (29.52%), Space: 80.1 MB (5.04%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        // Prime No start From 2
+        if( n < 2){
+            return 0;
+        }
+
+        boolean[] isComposite = new boolean [n];
+
+
+        for(int  i = 2 ; i*i < n ; i++){
+
+            if(!isComposite[i]){
+
+
+                for(int j = i*i ; j < n ; j +=i){ // Imp Condtn
+                    isComposite[j] = true;
+                }
+
+
+            }
+        }
+
+
+
+        int count = 0;
+
+
+        for(int i = 2 ; i < n ; i++){
+
+            if(!isComposite[i]){
+                count++;
+            }
+        }
+
+        return count;
+
+
+    }
+}
