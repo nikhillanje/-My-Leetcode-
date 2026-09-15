d3a3ebd7a98048225410469b89c676d79504dfdc
Nikhil Lanje <nikhillanje00@gmail.com>
2026-09-15 08:46:47 +0530

    Time: 6 ms (90.61%), Space: 57.6 MB (83.13%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,35 +1,59 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
 
-        List<Integer> ar = new ArrayList<>();
+        // List<Integer> ar = new ArrayList<>();
 
-        HashMap<Integer , Integer > map = new HashMap<>();
+        // HashMap<Integer , Integer > map = new HashMap<>();
+
+        // for(int i = 0 ; i < nums.length ; i++){
+
+        //     int ele = nums[i];
+
+        //     if(!map.containsKey(ele)){
+
+        //         map.put(ele , 1);
+
+        //     }
+        //     else{
+        //         map.put(ele , map.get(ele)+1);
+        //     }
+        // }
+
+        // for(int e : map.keySet()){
+
+        //     if(map.get(e) == 2){
+        //         ar.add(e);
+        //     }
+
+
+
+        // }
+
+        // return ar;
+
+
+
+        List<Integer> ar = new ArrayList<>();
 
         for(int i = 0 ; i < nums.length ; i++){
 
-            int ele = nums[i];
+            int idx = Math.abs(nums[i]) - 1;
 
-            if(!map.containsKey(ele)){
-
-                map.put(ele , 1);
-
+            if(nums[idx] < 0){
+                ar.add(idx+1);
             }
-            else{
-                map.put(ele , map.get(ele)+1);
-            }
+
+
+            nums[idx] = nums[idx] * -1;
         }
 
-        for(int e : map.keySet()){
-
-            if(map.get(e) == 2){
-                ar.add(e);
-            }
-
-
-
-        }
 
         return ar;
+
+
+
         
     }
 }
+
+
